2819dd561529b74991a9f9dee7d278d25c747c4b
MFAthesecond <141055098+MFAthesecond@users.noreply.github.com>
2023-12-29 22:45:10 +0300

    Add files via upload

---
+++ Question_09_13/Location.java
@@ -0,0 +1,21 @@
+package week_09.Question_09_13;
+
+public class Location {
+    int row;
+    int column;
+    double maxValue = Double.MIN_VALUE;
+
+    public static Location locateLargest(double[][] a) {
+        Location location = new Location();
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                if (a[i][j] > location.maxValue) {
+                    location.maxValue = a[i][j];
+                    location.row = i;
+                    location.column = j;
+                }
+            }
+        }
+        return location;
+    }
+}
